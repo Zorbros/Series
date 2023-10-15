70535d392618e9a57a3ec4de4f5781600eb6f8a8
Zobros <erkhembatu@gmail.com>
2023-10-15 16:12:26 +0800

    I
    10% DONE

---
+++ script.js
@@ -0,0 +1,25 @@
+function toggle(){
+    var story = document.getElementById("story");
+
+    if(story.style.display === "none"){
+        story.style.display = "block";
+    }
+
+    else{
+        story.style.display = "none";
+    }
+}
+
+function togglefirst(){
+    var About = document.getElementById("About");
+
+    if(About.style.display === "none"){
+        About.style.display = "block";
+    }
+
+    else{
+        About.style.display = "none";
+    }
+}
+
+/// /// /// ///
